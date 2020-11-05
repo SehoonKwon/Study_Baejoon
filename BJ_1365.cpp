32e18ddbb81ca6d95265e1e283d1e4d17df18ad9
SehoonKwon <49121446+SehoonKwon@users.noreply.github.com>
2020-11-05 22:41:05 +0900

    Create BJ_1365.cpp

---
+++ BJ_1365.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N;
+int Array[100001];
+vector<int> v;
+int len;
+
+void input()
+{
+	cin >> N;
+	for (int i = 0; i < N; i++)
+		cin >> Array[i];
+}
+
+void solve()
+{
+	v.push_back(Array[0]);
+	for (int i = 1; i < N; i++)
+	{
+		if (v[len] < Array[i])
+		{
+			v.push_back(Array[i]);
+			len++;
+		}
+		else
+		{
+			int idx = lower_bound(v.begin(), v.end(), Array[i]) - v.begin();
+			v[idx] = Array[i];
+		}
+	}
+	cout << N - v.size();
+}
+
+int main()
+{
+	std::ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	input();
+	solve();
+	return 0;
+}
