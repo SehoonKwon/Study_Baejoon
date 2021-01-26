1a43c690fac28556c742349888ad1bb14ce54308
SehoonKwon <49121446+SehoonKwon@users.noreply.github.com>
2021-01-26 22:16:46 +0900

    Create BJ_1052.cpp
    구현/시뮬레이션 문제인척 해놓고 수학적 아이디어 문제, 아래는 풀이 설명
    
    N=3일경우
    [1, 1, 1]  => [2, 1] 로 합칠 수 있다.
    
    N=5일 경우
    [1, 1, 1, 1, 1] => [2, 2, 1] => [4, 1]로 합칠 수 있다.
    
    N= 9일 경우
    [1, 1, 1, 1, 1, 1, 1, 1, 1] => [2, 2, 2, 2, 1] => [4, 4, 1] => [8, 1]로 합칠 수 있다.
    
    결과적으로 2의 i승이 되는 물의 양으로는 물병을 얼마든지 합칠 수 있다. 1L로 남는 수가 합칠 수 없는 물병이 되는 것이다. 
    이를 조금 더 수학적으로 풀어보면 N을 2진법으로 변환했을 때 1로 표현되는 수가 최대한 합치고 난 후 물병의 개수가 되는 것이다.
    예를들어, 9을 2진법으로 변환하면 1001이고, 이는 9의 경우 두개의 물병으로 합칠 수 있음을 의미한다 (8L 1개, 1L 1개)
    다른 예시로 7을 2진법으로 변환하면 111이고, 이는 7은 세개의 물병으로 합칠 수 있음을 의미한다. (4L, 2L, 1L 각각 1개씩)
    그래서 우리는 N의 값을 ++해가면서 필요한 물병의 개수가 K개를 넘지 않는 최댓값을 찾으면 되는 것이다.
    
    https://qgqg264.tistory.com/57 님 블로그 참고

---
+++ BJ_1052.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+//구현 문제가 아니라 사실 수학 문제임. 구현으로 풀면 시간초과
+
+int N, K;
+
+int main()
+{
+	cin >> N >> K;
+	int cnt = 0;
+	while (1)
+	{
+		int temp = N;
+		int tcnt = 0;
+		while (temp > 0)
+		{
+			if (temp % 2) tcnt++;
+			temp /= 2;
+		}
+
+		if(tcnt <= K) break;
+		N++;
+		cnt++;
+	}
+	cout << cnt;
+	return 0;
+}
+
+///////////////////////////////////////// 아래는 2진수를 변환(bitset이용)해서 하는건데 매우 느림. 시간초과 걸릴뻔
+//#include <iostream>
+//#include <bitset>
+//using namespace std;
+//
+////구현 문제가 아니라 사실 수학 문제임. 구현으로 풀면 시간초과
+//
+//int N, K;
+//
+//int main()
+//{
+//	cin >> N >> K;
+//	int cnt = 0;
+//	while (1)
+//	{
+//		string s = bitset<30>(N).to_string(); // 10^7인 2진수 아무거나 해도 되는데 그냥 크게 2^30으로 잡음
+//		int chk = 0;
+//		for (int i = 0; i < s.size(); i++)
+//			if (s[i] == '1') chk++;
+//
+//		if (chk <= K) break;
+//		N++;
+//		cnt++;
+//	}
+//	cout << cnt;
+//	return 0;
+//}
