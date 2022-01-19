08536abf61ed20f79cfad5830759b6d8a4dd95be
SehoonKwon <49121446+SehoonKwon@users.noreply.github.com>
2022-01-19 20:00:18 +0900

    [Bronze II] Title: 홀수일까 짝수일까, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/5988.홀수일까 짝수일까/홀수일까 짝수일까.cc
+++ 백준/Bronze/5988.홀수일까 짝수일까/홀수일까 짝수일까.cc
@@ -2,20 +2,18 @@
 #include <string>
 using namespace std;
 
+int N;
+string str;
+
 int main()
 {
-    int N;
     cin >> N;
-    
-    string s;
     while(N--)
     {
-        cin >> s;
-        char x = s[s.size()-1];
-        if(x == '2' || x == '4' || x == '6' || x == '8' || x == '0')
-            cout<<"even\n";
-        else
-            cout<<"odd\n";
+        cin >> str;
+        char X = str[str.size()-1];
+        if(X == '1' || X == '3' || X == '5' || X == '7' || X == '9') cout<<"odd\n";
+        else cout<<"even\n";
     }
     return 0;
 }
