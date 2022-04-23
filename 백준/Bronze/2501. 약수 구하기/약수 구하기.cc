10e2feb8a0afeb6c632dfdbc0d5e3ac491f66226
SehoonKwon <49121446+SehoonKwon@users.noreply.github.com>
2022-04-24 00:25:00 +0900

    [Bronze III] Title: 약수 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N,K;
+	cin>>N>>K;
+	int cnt = 0;
+	for(int i=1; i<=N; i++)
+	{
+		if(N%i == 0)
+		{
+			cnt++;
+			if(cnt == K)
+			{
+				cout<<i;
+				return 0;
+			}
+		}
+	}
+	cout<<0;
+	return 0;
+}
