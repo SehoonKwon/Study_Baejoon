497b6f4a3dd9ac6c72e8dc65cb2fe5839832980a
SehoonKwon <49121446+SehoonKwon@users.noreply.github.com>
2022-05-08 22:47:19 +0900

    [Bronze IV] Title: Counting Clauses, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/17903. Counting Clauses/Counting Clauses.cc
+++ 백준/Bronze/17903. Counting Clauses/Counting Clauses.cc
@@ -6,9 +6,7 @@ int Array[21][21];
 
 int main(){
     cin >> N >> M;
-    for(int i = 0; i < N; i++)
-        for(int j = 0; j < M; j++)
-            cin >> Array[i][j];
     if(N >= 8) cout << "satisfactory";
     else cout << "unsatisfactory";
+    return 0;
 }
