b47829be06fd7cfd7524359ebd08e5c5d520a4b8
SehoonKwon <49121446+SehoonKwon@users.noreply.github.com>
2022-05-28 01:28:14 +0900

    [Bronze IV] Title: Betting, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24751. Betting/Betting.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int A;
+	cin >> A;
+	int B = 100 - A;
+	
+	cout<<double(100)/double(A)<<endl;
+	cout<<double(100)/double(B);
+	
+	return 0;
+}
