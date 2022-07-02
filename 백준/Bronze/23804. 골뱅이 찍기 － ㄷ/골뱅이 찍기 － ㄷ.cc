45f8bd044c8222d014b4cf7b1b626bd1efc9bc4b
SehoonKwon <49121446+SehoonKwon@users.noreply.github.com>
2022-07-03 01:17:09 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㄷ, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23804. 골뱅이 찍기 － ㄷ/골뱅이 찍기 － ㄷ.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N;
+	cin>>N;
+	
+	for(int i=1; i<=N; i++){
+		for(int j=1; j<=N*5; j++)
+			cout<<"@";
+		cout<<"\n";
+	}
+	
+	for(int i=N+1; i<=N*4; i++){
+		for(int j=1; j<=N; j++)
+			cout<<"@";
+		cout<<"\n";
+	}
+	
+	for(int i=N*4+1; i<=N*5; i++){
+		for(int j=1; j<=N*5; j++)
+			cout<<"@";
+		cout<<"\n";
+	}
+	
+	return 0;
+}
