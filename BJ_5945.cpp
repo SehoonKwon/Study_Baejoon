63e0034e5cfe4b6fb9bab511f18749875f35e5a4
SehoonKwon <49121446+SehoonKwon@users.noreply.github.com>
2022-08-17 21:05:53 +0900

    Create BJ_5945.cpp

---
+++ BJ_5945.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	string s;
+	cin >> s;
+
+	int cnt = 0;
+	int len = s.length();
+	string nS = "";
+	for (int i = len - 1; i >= 0; i--) {
+		if (cnt == 3) {
+			nS += ",";
+			cnt = 0;
+		}
+
+		nS += s[i];
+		cnt++;
+	}
+	reverse(nS.begin(), nS.end());
+	cout << nS << "\n";
+
+	return 0;
+}
