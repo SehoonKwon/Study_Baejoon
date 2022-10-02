c1ebae41adba98ba8ab29c6b3670555cfbe2e6fc
SehoonKwon <49121446+SehoonKwon@users.noreply.github.com>
2022-10-02 20:43:11 +0900

    [Bronze IV] Title: Schronisko, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8760. Schronisko/Schronisko.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int T;
+    cin>>T;
+    while(T--){
+        int W, K;
+        cin >> W >> K;
+        cout<<W*K/2<<"\n";
+    }
+    return 0;
+}
