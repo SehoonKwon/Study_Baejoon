ebced11d45c90bf5dfdc1c9016f60adc2db8ada9
SehoonKwon <49121446+SehoonKwon@users.noreply.github.com>
2022-10-13 18:29:22 +0900

    Create BJ_24072.cpp

---
+++ BJ_24072.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int A, B, C;
+	cin >> A >> B >> C;
+	
+	if(A > C) cout<<0;
+	else{
+		if(C < B) cout<<1;
+		else cout<<0;
+	}
+	
+	return 0;
+}
