f27a17a9b5d74c526f81679ecd7273ec2d8f62c6
SehoonKwon <49121446+SehoonKwon@users.noreply.github.com>
2022-11-30 00:01:14 +0900

    [Bronze III] Title: 페르시아의 왕들, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10599. 페르시아의 왕들/페르시아의 왕들.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    while(1){
+        int a,b,c,d;
+        cin >> a >> b >> c >> d;
+        if(!a && !b && !c && !d ) break;        
+        cout<<c-b<<" "<<d-a<<"\n";
+    }
+    return 0;
+}
