ec3e306bd2a1c5ec0cd9b18927bd6ca486420c8f
SehoonKwon <49121446+SehoonKwon@users.noreply.github.com>
2022-12-19 07:17:33 +0900

    [Bronze IV] Title: Squares, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/6887. Squares/Squares.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    
+    printf("The largest square has side length %d.", (int)sqrt(N));
+
+    return 0;
+}
