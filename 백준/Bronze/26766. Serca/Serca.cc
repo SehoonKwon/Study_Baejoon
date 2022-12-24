eb0a5d127fcec1b2cc1da8f03b844e706012cb99
SehoonKwon <49121446+SehoonKwon@users.noreply.github.com>
2022-12-24 12:20:43 +0900

    [Bronze V] Title: Serca, Time: 132 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		cout << " @@@   @@@\n";
+		cout << "@   @ @   @\n";
+		cout << "@    @    @\n";
+		cout << "@         @\n";
+		cout << " @       @ \n";
+		cout << "  @     @  \n";
+		cout << "   @   @   \n";
+		cout << "    @ @    \n";
+		cout << "     @     \n";
+	}
+	return 0;
+}
